97c8012bea4f0fe6070d3484915cdea3d8f8fe4c
YONG-WANG-BRAVE <805549946@qq.com>
2022-02-08 14:12:40 +0800

    过滤器的实现，拦截请求头的内容

--- src/main/java/com/wy/demo/filter/headToSession/filterAddToSession.java
+++ src/main/java/com/wy/demo/filter/headToSession/filterAddToSession.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Service
-/**  过滤器的实现   过滤器的实现
+/**  过滤器的实现   过滤器的实现  过滤器的实现
  * 要实现从session中获取参数，处理后，放到session中
  * 1 继承OncePerRequestFilter，获取请求头的内容，放入session,放入拦截器链
  * 2 config 内容应该没用
