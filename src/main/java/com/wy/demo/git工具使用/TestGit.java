dffa226748835afe2d4dd985656f920b3412c820
YONG-WANG-BRAVE <805549946@qq.com>
2022-03-31 17:04:42 +0800

    fe2继续开发

--- src/main/java/com/wy/demo/git工具使用/TestGit.java
+++ src/main/java/com/wy/demo/git工具使用/TestGit.java
@@ -11,5 +11,6 @@ public class TestGit {
         System.out.println("feature1");
         System.out.println("feature1继续开发");
         System.out.println("featur2");
+        System.out.println("featur2继续开发");
     }
 }
