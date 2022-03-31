5c52c94f3c22b7ac0ea5699954acbf97efd97d56
YONG-WANG-BRAVE <805549946@qq.com>
2022-03-31 16:25:16 +0800

    git工具使用 测试git rebash c d e

--- src/main/java/com/wy/demo/git工具使用/TestGit.java
+++ src/main/java/com/wy/demo/git工具使用/TestGit.java
@@ -4,5 +4,9 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("a");
         System.out.println("b");
+        System.out.println("c");
+        System.out.println("d");
+        System.out.println("e");
+
     }
 }
