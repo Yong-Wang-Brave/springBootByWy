06e42a42ecf21edecafd4142d27f12d359ce6d45
YONG-WANG-BRAVE <805549946@qq.com>
2022-04-01 01:55:05 +0800

    git

--- src/main/java/com/wy/demo/gitOne/Sucess.java
+++ src/main/java/com/wy/demo/gitOne/Sucess.java
@@ -2,7 +2,7 @@ package com.wy.demo.gitOne;
 
 public class Sucess {
     public static void main(String[] args) {
-        System.out.println("2");
-        System.out.println("3");
+        System.out.println("提交1");
+
     }
 }
